7d3e54ae71e8cd5a9aa1d0a04f48a0dba6d6e7b7
Steven Freed <steven.freed@rutgers.edu>
2018-11-10 19:57:50 -0500

    commit

--- backend/api.js
+++ backend/api.js
@@ -7,11 +7,11 @@ var router = express.Router();
 
 // MySQL Create a Pool for Connection
 var pool = mysql.createPool({
-        host: config.host,
-        user: config.user,
-        port: config.port,
-        password: config.password,
-        database: config.database,
+        host: process.env.RDS_HOST,
+        user: process.env.RDS_USERNAME,
+        port: process.env.RDS_PORT,
+        password: process.env.RDS_PASSWORD,
+        database: 'BarBeerDrinkersPLUS',
 	      multipleStatements: true
 });
 
