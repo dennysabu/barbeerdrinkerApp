ea8f431b188156262bc44948d3bb0ecda17c38b0
EC2 Default User <ec2-user@ip-172-31-34-24.ec2.internal>
2018-11-12 16:22:03 +0000

    Denny test frontend

--- frontend/app/src/components/Beer.js
+++ frontend/app/src/components/Beer.js
@@ -5,6 +5,14 @@ class Beer extends Component {
     return (
       <div>
         <h2>Beer</h2>
+	<p>Hey Steve, don't yell at me</p>
+	<ul> 
+		<li> List of all beers</li>
+			<p> SELECT DISTINCT name from Items where type='beer' </p>
+		<li> Beers sold by certain bar </li>
+			<p> drop down of all bars, then get SELECT DISTINCT item from Sells, Items where Bar = '207' and Items.name = Sells.item and Items.type = 'beer' </p>
+		<li> Number of specific beer sold </li>
+	</ul>
       </div>
     );
   }
