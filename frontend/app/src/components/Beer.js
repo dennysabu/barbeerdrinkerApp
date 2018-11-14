a1f19ddd34d6a1f63d5fd3142b42eac956cbb0f0
dennysabu <36450431+dennysabu@users.noreply.github.com>
2018-11-14 15:53:21 -0500

    Add files via upload

--- frontend/app/src/components/Beer.js
+++ frontend/app/src/components/Beer.js
@@ -7,12 +7,17 @@ import {
     Button,
     Table,
 } from 'reactstrap'; // Table pre-built component from reactstrap library
-import './Beer.css'
-
-
+import './Beer.css';
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  HorizontalGridLines,
+  VerticalBarSeries,
+  VerticalGridLines,
+} from "react-vis";
 
 class Beer extends Component {
-
 
 
 
@@ -27,76 +32,41 @@ class Beer extends Component {
     tableHeaders: [],
     beerTableHeader: ["drinker", "bought"],
     isLoading: true,
-    BeerIsLoading: true,
+    graphItems: [],
+    graphLoading: true,
+    graphBarItems: [],
+    topBarData: [],
   };
 
     this.getBeers = this.getBeers.bind(this);
     this.getTopConsumers = this.getTopConsumers.bind(this)
     this.beerSelectionChanged = this.beerSelectionChanged.bind(this);
-    this.getSetInit = this.getSetInit.bind(this);
+    this.populateGraph = this.populateGraph.bind(this);
+    this.getTopBars = this.getTopBars.bind(this);
+    this.populateBarGraph = this.populateBarGraph.bind(this);
 
 
   }
 
 
 
-getTopConsumers(value){
-  console.log("From con " + value);
-  fetch('http://ec2-18-206-201-243.compute-1.amazonaws.com:5000/api/getTopBeerConsumers',{
-    method: "post",
-    headers: {
-        "Content-Type":"application/json",
-    },
-    body: JSON.stringify({
-      beer: value
-    })
-  }).then(res => res.json()
-).then(data=> this.setState({BeerData: data,  BeerIsLoading: false }));
-}
+
 
 
 componentDidMount() {
+
   this.getBeers();
-}
-
-beerSelectionChanged(e){
-  //console.log(e.target.value);
-  this.setState({Beer: e.target.value});
-  for (var i in this.state.data){
-
-    if(e.target.value === this.state.data[i].Beer){
-      this.setState({Manufacturer: this.state.data[i].Manufacturer});
-    }
-  }
-  this.getTopConsumers(e.target.value);
+  this.getTopConsumers(this.state.Beer);
 }
 
 
+componentDidUpdate(){
 
-
-getBeers(){
-
-  fetch('http://ec2-18-206-201-243.compute-1.amazonaws.com:5000/api/query',{
-    method: "post",
-    headers: {
-      "Content-Type":"application/json",
-    },
-    body: JSON.stringify({
-      "query": "SELECT DISTINCT name as Beer, manf as Manufacturer FROM Items WHERE type='beer'",
-    }),
-  }).then(res => res.json()
-).then(data => this.setState({ data: data, tableHeaders:  Object.keys(data[0]), isLoading: false, Beer: data[0].Beer, Manufacturer: data[0].Manufacturer}));
-this.getSetInit();
-
-}
-
-getSetInit(){
-  console.log(this.state.BeerData[0]);
-  this.getTopConsumers(this.state.BeerData[0]);
 }
 
 
   render(){
+
 
 
     if(this.state.isLoading){
@@ -107,6 +77,9 @@ getSetInit(){
       )
     } else {
 
+
+
+      let barTableData = [];
 
       const table = <Table>
                         <thead style={{fontSize:'22px', textAlign:'center'}}>
@@ -170,45 +143,82 @@ getSetInit(){
               })
             }
           </tbody>
-
-
         </Table>
+
+      const x =    <XYPlot animation={true} margin={{bottom: 50}} xType="ordinal" width={1000} height={500}>
+                    <VerticalGridLines />
+                    <HorizontalGridLines />
+                    <XAxis/>
+                    <YAxis title="Quantity" />
+                    <VerticalBarSeries data={this.state.graphItems} color="skyblue"/>
+                  </XYPlot>
+
+      const y =    <XYPlot animation={true} margin={{bottom: 50}} xType="ordinal" width={1000} height={500}>
+                    <VerticalGridLines />
+                    <HorizontalGridLines />
+                    <XAxis/>
+                    <YAxis title="Quantity"/>
+                    <VerticalBarSeries data={this.state.graphBarItems} color="skyblue"/>
+                  </XYPlot>
 
 //Actual HTML
         return (
           <div className="glb">
 
-          <div className="firstTable">
-              <div className="tlb_sel">
-                  <p> Select Beer </p>
-                  <Input type="select" name="select" onChange={this.beerSelectionChanged} >
-                  {
-                    this.state.data.map(Beer =>
-                      <option key={Beer.item}>
-                        {Beer.Beer}
-                      </option>
-                    )
-                  }
-              </Input>
+            <div className="firstTable">
+                <div className="tlb_sel">
+                    <p> Select Beer </p>
+                    <Input type="select" name="select" onChange={this.beerSelectionChanged} >
+                    {
+                      this.state.data.map(Beer =>
+                        <option key={Beer.item}>
+                          {Beer.Beer}
+                        </option>
+                      )
+                    }
+                </Input>
+
+                </div>
+
+
+                <div className="tlb">
+
+                  {table}
+                </div>
+            </div>
+
+            <div className="beerinsight">
+                    <div className ="insightHeader">
+                      <h1> Beer Insight </h1>
+                      <h2> Beer: {this.state.Beer} </h2>
+                      <h2> Manufacturer: {this.state.Manufacturer} </h2>
+                      <hr/>
+                    </div>
+
+              <div className="actualInsights">
+                    <div className="TopBuyers">
+                            <h1 className="beerinsightgraphtitle"> Top 10 Buyers of {this.state.Beer}</h1>
+                            {x}
+                            <hr/>
+                            {insights}
+                    </div>
+                    <hr/>
+                    <div className="TopSellers">
+                            <h1 className="beerinsightgraphtitle"> Top 10 Selling Bars for {this.state.Beer}</h1>
+                            {y}
+                            <hr/>
+
+
+
+                    </div>
+
 
               </div>
 
 
-              <div className="tlb">
-
-                {table}
               </div>
 
-          </div>
 
-          <div className="beerinsight">
-
-            <h1> Beer Insight </h1>
-            <h2> Beer: {this.state.Beer} </h2>
-            <h2> Manufacturer: {this.state.Manufacturer} </h2>
-            {insights}
-
-          </div>
 
           </div >
 
@@ -223,11 +233,126 @@ getSetInit(){
 }//ends return
 
 
+populateBarGraph(data)
+{
+  let g = [];
+
+  if(data.length === 0)
+      {
+      g[0] = {x: "None", y: 0};
+      this.setState({graphBarItems: g});
+      //this.setState({graphItems: {x: "None", y: 0} });
+      return;
+      }
+    else{
+      for(var i in data){
+        g[i] = {x: data[i].bar, y: data[i].sold}
+        }
+      }
+      this.setState({graphBarItems:g});
+      this.forceUpdate();
+
+      return;
+}
+
+
+getTopBars(value){
+
+
+  fetch('http://ec2-18-206-201-243.compute-1.amazonaws.com:5000/api/getBarsForBeer',{
+    method: "post",
+    headers: {
+        "Content-Type":"application/json",
+    },
+    body: JSON.stringify({
+      beer: value
+    })
+  }).then(res => res.json(),
+  ).then(data=> {
+  this.setState({topBarData: data});
+  this.populateBarGraph(data);
+  } );
+
+}
 
 
 
 
 
+
+populateGraph(data){
+
+  let g = [];
+
+  if(data.length === 0)
+      {
+      g[0] = {x: "None", y: 0};
+      this.setState({graphItems: g});
+      //this.setState({graphItems: {x: "None", y: 0} });
+      return;
+      }
+    else{
+      for(var i in data){
+        g[i] = {x: data[i].drinker, y: data[i].bought}
+        }
+      }
+      this.setState({graphItems:g});
+      this.forceUpdate();
+
+      return;
+
+
+}
+
+getTopConsumers(value){
+
+  fetch('http://ec2-18-206-201-243.compute-1.amazonaws.com:5000/api/getTopBeerConsumers',{
+    method: "post",
+    headers: {
+        "Content-Type":"application/json",
+    },
+    body: JSON.stringify({
+      beer: value
+    })
+  }).then(res => res.json(),
+).then(data=> {
+  this.setState({BeerData: data,  BeerIsLoading: false});
+  this.populateGraph(data);
+  this.getTopBars(value);
+} );
+
+  this.forceUpdate();
+}
+
+
+getBeers(){
+
+  fetch('http://ec2-18-206-201-243.compute-1.amazonaws.com:5000/api/query',{
+    method: "post",
+    headers: {
+      "Content-Type":"application/json",
+    },
+    body: JSON.stringify({
+      "query": "SELECT DISTINCT name as Beer, manf as Manufacturer FROM Items WHERE type='beer'",
+    }),
+      }).then(res => res.json()
+          ).then(data => this.setState({ data: data, tableHeaders:  Object.keys(data[0]), isLoading: false, Beer: data[0].Beer, Manufacturer: data[0].Manufacturer}));
+}
+
+
+
+beerSelectionChanged(e){
+  //console.log(e.target.value);
+  this.setState({Beer: e.target.value});
+  for (var i in this.state.data){
+
+    if(e.target.value === this.state.data[i].Beer){
+      this.setState({Manufacturer: this.state.data[i].Manufacturer});
+    }
+  }
+  this.getTopConsumers(e.target.value);
+
+}
 
 
 }
