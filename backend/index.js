6020a4dbeea088aa3b52a0f66c4bf74f621f5cd7
Steven Freed <steven.freed@rutgers.edu>
2018-11-13 23:41:38 -0500

    Modifications Page done, add pop ups to notify successful modifications

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,10 @@ var cors = require('cors');
 var express = require('express');
 var bodyParser = require('body-parser');
 
-// app 
+// app
 var app = express();
 
-app.options('*', cors()); 
+app.options('*', cors());
 app.use(cors());
 app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
