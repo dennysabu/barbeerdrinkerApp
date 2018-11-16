6df3f87fa3db5bc48536087f6dcd5701610f17f0
Steven Freed <steven.freed@rutgers.edu>
2018-11-16 15:27:08 -0500

    trigs

--- Database/SteveTrigs.sql
+++ Database/SteveTrigs.sql
@@ -1,18 +1,13 @@
-# Makes sure bartenders cannot work more than one shift per day
+# Makes sure bartender works at bar where bill is issued, for Bills insert
 DELIMITER $
-CREATE TRIGGER BartendersShiftsFK
-BEFORE INSERT ON Shifts
+CREATE TRIGGER BartendersShifts_Bills
+BEFORE INSERT ON Bills
 FOR EACH ROW BEGIN
-if (NOT EXISTS(SELECT b.name, b.city, bt.name, bt.city
-FROM Bars b, Bartenders bt
-WHERE b.city = bt.city AND b.name = NEW.bar AND bt.name = NEW.bartender)) THEN
+if (NOT EXISTS(SELECT * FROM Shifts s 
+WHERE NEW.bar = s.bar AND s.bartender = NEW.bartender AND DATE(NEW.date) = s.day AND
+TIME(NEW.date) BETWEEN s.startTime AND s.endTime)) THEN
 		SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Cannot insert, update, or delete in Shifts: a bartender cannot work more than one shift per day';
+        SET MESSAGE_TEXT = 'Cannot insert, update, or delete in Shifts: this bartender does/is not working at the listed bar at this time on this date';
 END IF;
 END $
 DELIMITER ;
-
-SELECT s.bartender, s.bar, s.day, COUNT(s.day) as shifts_worked
-FROM Shifts s 
-GROUP BY s.bartender, s.day
-HAVING shifts_worked = 1;
--- Database/TriggerScripts.sql
+++ Database/TriggerScripts.sql
@@ -30,3 +30,18 @@ WHERE b.city = bt.city AND b.name = NEW.bar AND bt.name = NEW.bartender)) THEN
 END IF;
 END $
 DELIMITER ;
+
+# Makes sure bartender works at bar where bill is issued, for Bills insert
+DELIMITER $
+CREATE TRIGGER BartendersShifts_Bills
+BEFORE INSERT ON Bills
+FOR EACH ROW BEGIN
+if (NOT EXISTS(SELECT * FROM Shifts s 
+WHERE NEW.bar = s.bar AND s.bartender = NEW.bartender AND DATE(NEW.date) = s.day AND
+TIME(NEW.date) BETWEEN s.startTime AND s.endTime)) THEN
+		SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Cannot insert, update, or delete in Shifts: this bartender does/is not working at the listed bar at this time on this date';
+END IF;
+END $
+DELIMITER ;
+
