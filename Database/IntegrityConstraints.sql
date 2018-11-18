460a8b8294349d922f50f6f1e95d03b086199db8
Denny Sabu <denny.sabu@rutgers.edu>
2018-11-18 16:13:44 -0500

    mama is a saint she raised me real good

--- Database/IntegrityConstraints.sql
+++ Database/IntegrityConstraints.sql
@@ -8,11 +8,25 @@ AND
 b.city != d.city;
 
 -- Verify Bar A and Bar B
-SELECT s1.item, s1.bar, s1.price, s2.item, s2.bar, s2.price,s3.item, s3.bar, s3.price, s4.item, s4.bar, s4.price
-FROM Sells s1, Sells s2, Sells s3, Sells s4
-WHERE s1.bar = s2.bar AND s1.bar = s3.bar AND s4.bar = s3.bar
-AND s1.item <> s2.item AND s1.item = s3.item AND s2.item = s4.item
-AND s1.price > s2.price AND s3.price < s4.price;
+SELECT 
+    *
+FROM
+    Sells s1,
+    Sells s2,
+    Sells s3,
+    Sells s4
+WHERE
+    s1.day = s2.day AND s3.day = s4.day
+        AND s1.day = s3.day
+        AND s1.item = s3.item
+        AND s2.item = s4.item
+        AND s1.bar = s3.bar
+        AND s2.bar = s4.bar
+        AND s1.price > s3.price
+        AND s2.price < s4.price
+;
+
+
 
 -- Verify Bill is made when bar is open
 SELECT bi.date, ba.weekendOpen, ba.weekendClose, bi.bar
